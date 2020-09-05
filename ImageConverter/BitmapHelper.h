323b158dce015e61641361d05332b7de97ff2116
minjasilven <minja.silvennoinen@futurice.com>
2020-09-05 15:28:28 +0300

    More refactoring and moving stuff to bitmap helper class

--- ImageConverter/BitmapHelper.h
+++ ImageConverter/BitmapHelper.h
@@ -21,5 +21,12 @@ public:
 	//void loadBitmapFromFolder();
 	//void readBitmapFileHeader();
 
+	unsigned char* data_pix;
+	unsigned char* data_pix_from_saved_bitmap;
+	int size;
+	int totalSize;
+
 	void saveBitmapValues(int width, int height,int bytesPerPixel, unsigned char *pixelData);
+	void readBitmapImageFromFile(const char *fileName, int *width, int *height, int *bytesPerPixel);
+	void writeBitmap(const char *fileName, int width, int height, int bytesPerPixel);
 };
