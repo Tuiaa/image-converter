6e8d8bf23b18c28ed6a93aaf2421f32c501c3e41
minjasilven <minja.silvennoinen@futurice.com>
2020-09-07 13:49:13 +0300

    Bitmap.h refactored

--- ImageConverter/Bitmap.h
+++ ImageConverter/Bitmap.h
@@ -1,83 +1,62 @@
 #pragma once
 
 /*
- *	- Block of bytes is at the start of the bitmap file
- *	- Used to identify the file
- *	- It's size is 14 bytes
+ *		Bitmap File Header
+ *		- stores general information about the bitmap image file
+ *		- it's size is 14 bytes
  */
-struct BitmapFileHeaderStruct
+struct BitmapFileHeader
 {
 	// First 2 bytes are the characters of bitmap file are "B" and "M" in ASCII
-	//unsigned char fileTypeSignature[2] = { 'B', 'M' };
 	const char *BM = "BM";
-	// Next is the size of the BMP in bytes (Bitmap file header 14 bytes + DIB header 40 bytes)
-	//unsigned char sizeOfBitmapHeader = 14;
-
-	// Filesize in bytes
+	// Next is the size of the bitmap in bytes
 	int filesize;
-	// Next is reserved bytes, should be 0?
+	// Next is reserved bytes, should be 0
 	int reserved = 0x0000;
 	// Data offset, i.e. starting address of the byte where the bitmap image data (pixel array) can be found
 	int dataOffset;
 };
 
 /*
- *	- Bitmap information header
- *	- Tells the application detailed information about the image
- *	- Information is used to display the image on the screen
+ *		DIB Header (bitmap informaion header)
+ *		- Tells the application detailed information about the image
+ *		- Immediately follows bitmap file header
  */
-struct DIBHeaderStruct {
-	// 0E the size of this header
+struct DIBHeader {
+	// The size of this header
 	int infoHeaderSize;
-	// 12 the bitmap width in pixels
+	// Bitmap width in pixels
 	int width;
-	// 14 the bitmap height in pixels
+	// Bitmap height in pixels
 	int height;
-	// 16 the number of color planes, must be 1?
-	/*
-	 *	- Occurs after bitmap header and dib header (and optional extra bit masks)
-	 *	- Is a block of bytes (a table) listing the colors used by the image
-	 *	- Number of entries in the color table palette is either 2*n (where n is number of bits per pixel)
-	 *	  or a smaller number specified in the header
-	 */
+
+	// The number of color planes, must be 1
 	int colorPlanes = 1;
-	// 18 the number of bits per pixel
+	// The number of bits per pixel
 	int bitsPerPixel = 3;		// red, green, blue
 
-	//compression
+	//	Possible used compression method
 	int compression;
-
-	// image size
+	// The image size, the size of the raw bitmap data
 	int imageSize;
 
-	// horizontal reso
 	int horizontalResolution;
-	// vertical reso
 	int verticalResolution;
 
-	// number of colors in color palette --> 0 to default
+	// Number of colors in color palette --> 0 to default
 	int colorsUsedInColorPalette;
-	// num of important color used --> 0 when every color is important
+	// Number of important color used --> 0 when every color is important
 	int importantColors;
 };
 
 class Bitmap {
 public:
-	// Bitmap file header
-	BitmapFileHeaderStruct bitmapFileHeader;
-	// DIB header?
-	DIBHeaderStruct dibHeader;
+	BitmapFileHeader bitmapFileHeader;
+	DIBHeader dibHeader;
 
-	// Color table, its offset is the size of the BITMAPFILEHEADER + the size of the DIBHEADER (+ optional masks)
-	//COLORTABLE2 colorTable;
-
+	// Saved so no need to calculate again (= bitsPerPixel / 8)
 	int bytesPerPixel;
 	
-	/*
-	 *	- The bits representing the bitmap pixels are packed in rows
-	 *	- The size of each row is rounded up to a multiple of 4 bytes by padding
-	 *	- For images with height above 1, multiple padded rows are stored consecutively, forming a pixel array
-	 */
+	// Array containing all the pixels from image
 	unsigned char* pixelData;
-
 };
